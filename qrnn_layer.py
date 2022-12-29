1aa99a196cccb4605aae6bf3dd93c5bfb953cfd1
Leander Girrbach <9577191+LGirrbach@users.noreply.github.com>
2022-12-29 11:09:43 +0100

    Fix variable names

--- qrnn_layer.py
+++ qrnn_layer.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class QRNNLayer(nn.Module):
@@ -22,22 +21,24 @@ class QRNNLayer(nn.Module):
         
         # Apply convolutions
         inputs = inputs.transpose(1, 2)
-        f_raw = self.f_conv(inputs).transpose(1, 2)
-        z_raw = self.z_conv(inputs).transpose(1, 2)
+        raw_f = self.f_conv(inputs).transpose(1, 2)
+        raw_z = self.z_conv(inputs).transpose(1, 2)
         
         # Get log values of activations
         log_z = torch.log(torch.nn.functional.softplus(raw_z))
         log_f = torch.nn.functional.logsigmoid(raw_f)
         log_one_minus_f = torch.nn.functional.logsigmoid(-raw_f)
         
-        # Precalculate recurrent gate values
+        # Precalculate recurrent gate values by reverse cumsum
         recurrent_gates = log_f[:, 1:, :]
         recurrent_gates_cumsum = torch.cumsum(recurrent_gates, dim=1)
         recurrent_gates = recurrent_gates - recurrent_gates_cumsum + recurrent_gates_cumsum[:, -1:, :]
+        
+        # Pad last timestep
         padding = torch.zeros([batch, 1, self.hidden_size], device=recurrent_gates.device)
         recurrent_gates = torch.cat([recurrent_gates, padding], dim=1)
         
-        # Calculate recursion
+        # Calculate expanded recursion by cumsum (logcumsumexp in log space)
         log_hidden = torch.logcumsumexp(log_z + log_one_minus_f + recurrent_gates, axis=1)
         hidden = torch.exp(log_hidden - recurrent_gates)
         
